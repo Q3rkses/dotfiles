b7eef63e573184322ba31d6fc0113fea1bed69ff
Q3rkses <cyprian.osinski03@gmail.com>
2025-06-13 02:51:30 +0200

    switched to wofi & switched themes

--- .config/sh_scripts/bluelight-sunset-menu.sh
+++ .config/sh_scripts/bluelight-sunset-menu.sh
@@ -10,14 +10,14 @@ choices=(
 )
 
 selection=$(printf "%s\n" "${choices[@]}" \
-  | wofi --dmenu --prompt="Set screen temperature:" )
+  | rofi --dmenu --prompt="Set screen temperature:" )
 
 case "$selection" in
   *6500K) temperature=6500 ;;
   *4500K) temperature=4500 ;;
   *3400K) temperature=3400 ;;
   "Custom…")
-    temperature=$(printf "" | wofi --dmenu --prompt="Temperature in K:")
+    temperature=$(printf "" | rofi --dmenu --prompt="Temperature in K:")
     ;;
   *) exit 0 ;;
 esac
